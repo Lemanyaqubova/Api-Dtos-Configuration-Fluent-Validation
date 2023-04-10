d78087b4ddc729d99fa700ce152461e8f702890f
Lemanyaqubova <lamanny@code.edu.az>
2023-04-10 20:13:03 +0400

    end

---
+++ FirstApi/Dtos/ProductDto/ProductUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstApi.Dtos.ProductDto
+{
+    public class ProductUpdateDto
+    {
+
+        public string Name { get; set; }
+        public double SalePrice { get; set; }
+        public double CostPrice { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
